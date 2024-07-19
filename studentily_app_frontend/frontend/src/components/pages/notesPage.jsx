e293b1240d9faf0499e8debc9fe7ba48c385e906
Matthias <66430209+MATZup@users.noreply.github.com>
2024-07-19 21:31:17 +0200

    Update notesPage.jsx

--- studentily_app_frontend/frontend/src/components/pages/notesPage.jsx
+++ studentily_app_frontend/frontend/src/components/pages/notesPage.jsx
@@ -130,7 +130,7 @@ export default function NotesPage() {
           Notes
         </h1>
         <div className="flex justify-center items-center w-full h-screen">
-          <div className="w-full mb-[1.5rem]">
+          <div className="w-full mb-[1.55rem]">
             <div className="max-h-[calc(87vh-8rem)] w-full overflow-y-auto px-4">
               {allNotes.length > 0 ? (
                 <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
