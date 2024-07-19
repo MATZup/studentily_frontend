75a36926745a58d90664bac536f97382bb4fd81f
Matthias <66430209+MATZup@users.noreply.github.com>
2024-07-19 18:24:22 +0200

    Update productivityCards.jsx

--- studentily_app_frontend/frontend/src/components/home/productivityCards.jsx
+++ studentily_app_frontend/frontend/src/components/home/productivityCards.jsx
@@ -24,7 +24,7 @@ export default function productivityCards() {
               Notes
             </span>
 
-            <img className="w-[3.3rem] mb-[.3rem]" src={notesIcon} alt="" />
+            <img className="max-500:w-[2.7rem] max-500:bm-0 w-[3.3rem] mb-[.3rem]" src={notesIcon} alt="" />
           </div>
         </div>
         <div
@@ -39,7 +39,7 @@ export default function productivityCards() {
             <span className="text-3xl mb-[.4rem] font-bold text-[#B9514C]">
               Pomodoro
             </span>
-            <img className="w-[4.4rem] mb-[1.2rem]" src={pomodoroIcon} alt="" />
+            <img className="max-500:w-[2.7rem] max-500:bm-0 w-[4.4rem] mb-[1.2rem]" src={pomodoroIcon} alt="" />
           </div>
         </div>
         <div
@@ -54,7 +54,7 @@ export default function productivityCards() {
             <span className="text-3xl mb-[.8rem] font-bold text-[#72A889]">
               Todos
             </span>
-            <img className="w-16 mb-[.95rem]" src={todosIcon} alt="" />
+            <img className="max-500:w-[2.7rem] max-500:bm-0 w-16 mb-[.95rem]" src={todosIcon} alt="" />
           </div>
         </div>
         <div
@@ -69,7 +69,7 @@ export default function productivityCards() {
             <span className="text-3xl mb-[.8rem] font-bold text-[#736AB1]">
               Journal
             </span>
-            <img className="w-[3.5rem] mb-[.95rem]" src={journalIcon} alt="" />
+            <img className="max-500:w-[2.7rem] max-500:bm-0 w-[3.5rem] mb-[.95rem]" src={journalIcon} alt="" />
           </div>
         </div>
       </div>
