e1616356ecbb4410be090c04026f633cd9f6b596
Matthias <66430209+MATZup@users.noreply.github.com>
2024-08-12 15:09:20 +0200

    Update productivityCards.jsx

--- studentily_app_frontend/frontend/src/components/home/productivityCards.jsx
+++ studentily_app_frontend/frontend/src/components/home/productivityCards.jsx
@@ -16,6 +16,7 @@ export default function productivityCards() {
       >
         <button
           id="notesCard"
+          tabindex="1"
           className="w-80 h-40 max-720:h-[7.2rem] max-720:w-[26.5rem] max-767:w-[26rem] max-767:h-[7.5rem] max-767:hover:w-[27rem] max-767:hover:h-[8.5rem] max-970:w-[20rem] max-970:h-[10.5rem] max-970:hover:h-[11.5rem] max-970:hover:w-[21rem] max-1200:w-[23rem] max-1200:h-[12rem] max-1200:hover:h-[13rem] max-1200:hover:w-[24rem] max-1350:w-[26rem] max-1350:h-[13rem] max-1350:hover:w-[27rem] max-1350:hover:h-[14rem] bg-[#FFEB95] flex flex-col items-center justify-center rounded-2xl hover:w-[21rem] hover:h-[11rem] transition-all duration-300 drop-shadow-xl cursor-pointer"
           onClick={() => navigate("/notes")}
         >
@@ -29,6 +30,7 @@ export default function productivityCards() {
         </button>
         <button
           id="pomodoroCard"
+          tabindex="2"
           className="w-80 max-767:w-[26rem] max-767:h-[7.5rem] h-40 max-720:h-[7.2rem] max-720:w-[26.5rem] max-767:hover:w-[27rem] max-767:hover:h-[8.5rem] max-970:w-[20rem] max-970:h-[10.5rem] max-970:hover:h-[11.5rem] max-970:hover:w-[21rem] max-1200:w-[23rem] max-1200:h-[12rem] max-1200:hover:h-[13rem] max-1200:hover:w-[24rem] max-1350:w-[26rem] max-1350:h-[13rem] max-1350:hover:w-[27rem] max-1350:hover:h-[14rem] drop-shadow-xl bg-[#fa746d] cursor-pointer pt-3 flex flex-col items-center justify-center rounded-2xl hover:w-[21rem] hover:h-[11rem] transition-all duration-300"
           onClick={() => navigate("/pomodoro")}
         >
@@ -44,6 +46,7 @@ export default function productivityCards() {
         </button>
         <button
           id="todosCard"
+          tabindex="3"
           className="w-80 h-40 max-767:w-[26rem] max-767:h-[7.5rem] max-720:h-[7.2rem] max-720:w-[26.5rem] max-767:hover:w-[27rem] max-767:hover:h-[8.5rem] max-970:w-[20rem] max-970:h-[10.5rem] max-970:hover:h-[11.5rem] max-970:hover:w-[21rem] max-1200:w-[23rem] max-1200:h-[12rem] max-1200:hover:h-[13rem] max-1200:hover:w-[24rem] max-1350:w-[26rem] max-1350:h-[13rem] max-1350:hover:w-[27rem] max-1350:hover:h-[14rem] drop-shadow-xl bg-[#92E0B3] pt-3 cursor-pointer flex flex-col items-center justify-center rounded-2xl hover:w-[21rem] hover:h-[11rem] transition-all duration-300"
           onClick={() => navigate("/todos")}
         >
@@ -59,6 +62,7 @@ export default function productivityCards() {
         </button>
         <button
           id="journalCard"
+          tabindex="4"
           className="w-80 h-40 max-767:w-[26rem] max-767:h-[7.5rem] max-720:h-[7.2rem] max-720:w-[26.5rem] max-767:hover:w-[27rem] max-767:hover:h-[8.5rem] max-970:w-[20rem] max-970:h-[10.5rem] max-970:hover:h-[11.5rem] max-970:hover:w-[21rem] max-1200:w-[23rem] max-1200:h-[12rem] max-1200:hover:h-[13rem] max-1200:hover:w-[24rem] max-1350:w-[26rem] max-1350:h-[13rem] max-1350:hover:w-[27rem] max-1350:hover:h-[14rem] drop-shadow-xl bg-[#9D95DD] pt-3 cursor-pointer flex flex-col items-center justify-center rounded-2xl hover:w-[21rem] hover:h-[11rem] transition-all duration-300"
           onClick={() => navigate("/journal")}
         >
