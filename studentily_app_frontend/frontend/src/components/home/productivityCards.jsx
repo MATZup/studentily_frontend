304e57e0c2d2c73ba4d52e04a0f8de1f67eff270
Matthias <66430209+MATZup@users.noreply.github.com>
2024-08-12 15:53:16 +0200

    Update productivityCards.jsx

--- studentily_app_frontend/frontend/src/components/home/productivityCards.jsx
+++ studentily_app_frontend/frontend/src/components/home/productivityCards.jsx
@@ -14,8 +14,9 @@ export default function productivityCards() {
         id="cards_wrapper"
         className="max-767:-mt-[44px] between-768-1350:-mt-[2rem] max-767:-mt-16 max-767:px-6 max-970:gap-4 max-970:w-full max-970:mx-8 flex flex-wrap max-1350:justify-center max-1350:pr-0 max-900:justify-center pr-[7rem] z-[70] max-1200:w-[60rem] w-[70rem] min-1350:max-w-[50rem] absolute top-[35%] gap-6"
       >
-        <div
+        <button
           id="notesCard"
+          tabindex="1"
           className="max-580:hover:h-[5.6rem] max-767:h-[8rem] max-500:h-[5.6rem] w-80 h-40 max-720:h-[7.2rem] max-720:w-[26.5rem] max-767:w-[26rem] max-767:hover:w-[27rem] max-767:hover:h-[8.5rem] max-970:w-[20rem] max-970:h-[10.5rem] max-970:hover:h-[11.5rem] max-970:hover:w-[21rem] max-1200:w-[23rem] max-1200:h-[12rem] max-1200:hover:h-[13rem] max-1200:hover:w-[24rem] max-1350:w-[26rem] max-1350:h-[13rem] max-1350:hover:w-[27rem] max-1350:hover:h-[14rem] bg-[#FFEB95] flex flex-col items-center justify-center rounded-2xl hover:w-[21rem] hover:h-[11rem] transition-all duration-300 drop-shadow-xl cursor-pointer"
           onClick={() => navigate("/notes")}
         >
@@ -30,9 +31,10 @@ export default function productivityCards() {
               alt=""
             />
           </div>
-        </div>
-        <div
+        </button>
+        <button
           id="pomodoroCard"
+          tabindex="2"
           className="max-580:hover:h-[5.6rem] max-767:h-[8rem] max-500:h-[5.6rem] w-80 max-767:w-[26rem] h-40 max-720:h-[7.2rem] max-720:w-[26.5rem] max-767:hover:w-[27rem] max-767:hover:h-[8.5rem] max-970:w-[20rem] max-970:h-[10.5rem] max-970:hover:h-[11.5rem] max-970:hover:w-[21rem] max-1200:w-[23rem] max-1200:h-[12rem] max-1200:hover:h-[13rem] max-1200:hover:w-[24rem] max-1350:w-[26rem] max-1350:h-[13rem] max-1350:hover:w-[27rem] max-1350:hover:h-[14rem] drop-shadow-xl bg-[#fa746d] cursor-pointer pt-3 flex flex-col items-center justify-center rounded-2xl hover:w-[21rem] hover:h-[11rem] transition-all duration-300"
           onClick={() => navigate("/pomodoro")}
         >
@@ -49,9 +51,10 @@ export default function productivityCards() {
               alt=""
             />
           </div>
-        </div>
-        <div
+        </button>
+        <button
           id="todosCard"
+          tabindex="3"
           className="max-580:hover:h-[5.6rem] max-767:h-[8rem] max-500:h-[5.6rem] w-80 h-40 max-767:w-[26rem] max-720:h-[7.2rem] max-720:w-[26.5rem] max-767:hover:w-[27rem] max-767:hover:h-[8.5rem] max-970:w-[20rem] max-970:h-[10.5rem] max-970:hover:h-[11.5rem] max-970:hover:w-[21rem] max-1200:w-[23rem] max-1200:h-[12rem] max-1200:hover:h-[13rem] max-1200:hover:w-[24rem] max-1350:w-[26rem] max-1350:h-[13rem] max-1350:hover:w-[27rem] max-1350:hover:h-[14rem] drop-shadow-xl bg-[#92E0B3] pt-3 cursor-pointer flex flex-col items-center justify-center rounded-2xl hover:w-[21rem] hover:h-[11rem] transition-all duration-300"
           onClick={() => navigate("/todos")}
         >
@@ -68,9 +71,10 @@ export default function productivityCards() {
               alt=""
             />
           </div>
-        </div>
-        <div
+        </button>
+        <button
           id="journalCard"
+          tabindex="4"
           className="max-580:hover:h-[5.6rem] max-767:h-[8rem] max-500:h-[5.6rem] w-80 h-40 max-767:w-[26rem] max-720:h-[7.2rem] max-720:w-[26.5rem] max-767:hover:w-[27rem] max-767:hover:h-[8.5rem] max-970:w-[20rem] max-970:h-[10.5rem] max-970:hover:h-[11.5rem] max-970:hover:w-[21rem] max-1200:w-[23rem] max-1200:h-[12rem] max-1200:hover:h-[13rem] max-1200:hover:w-[24rem] max-1350:w-[26rem] max-1350:h-[13rem] max-1350:hover:w-[27rem] max-1350:hover:h-[14rem] drop-shadow-xl bg-[#9D95DD] pt-3 cursor-pointer flex flex-col items-center justify-center rounded-2xl hover:w-[21rem] hover:h-[11rem] transition-all duration-300"
           onClick={() => navigate("/journal")}
         >
@@ -87,7 +91,7 @@ export default function productivityCards() {
               alt=""
             />
           </div>
-        </div>
+        </button>
       </div>
     </div>
   );
