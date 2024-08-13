7a3383cb4f2c37409dbd2f6bb46e82653d999171
Matthias <66430209+MATZup@users.noreply.github.com>
2024-08-13 15:44:14 +0200

    Update EditAndAddNotes.jsx

--- studentily_app_frontend/frontend/src/components/Notes/EditAndAddNotes.jsx
+++ studentily_app_frontend/frontend/src/components/Notes/EditAndAddNotes.jsx
@@ -102,7 +102,7 @@ export default function EditAndAddNotes({
 
         <div className="flex text-start flex-col gap-2 mt-5">
           <input
-            tabIndex="7"
+            tabindex="7"
             type="text"
             className="text-2xl text-white bg-[#191A27] outline-none"
             placeholder="Title..."
@@ -113,7 +113,7 @@ export default function EditAndAddNotes({
 
         <div className="flex flex-col gap-2 mt-4">
           <textarea
-            tabIndex="8"
+            tabindex="8"
             type="text"
             className="text-sm max-650:max-h-[21rem] max-465:max-h-[18rem] max-385:max-h-[14rem] text-[#dfdfdf] rounded-xl outline-none bg-[#2D3046] p-3"
             placeholder="Content..."
@@ -136,7 +136,7 @@ export default function EditAndAddNotes({
           {error && <p className="text-red-600 text-xs pt-3">{error}</p>}
 
           <button
-            tabIndex="11"
+            tabindex="11"
             className="right-0 font-medium max-960:w-[7.5rem] w-[10rem] text-white rounded-full hover:bg-[#877fc5] transition-all p-1 bg-[#726AB1]"
             onClick={noteAddHandler}
           >
