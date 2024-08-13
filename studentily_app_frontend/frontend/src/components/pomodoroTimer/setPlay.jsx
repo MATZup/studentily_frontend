ad8cec00e39ed34a7d790439bf7bc729d5392195
Matthias <66430209+MATZup@users.noreply.github.com>
2024-08-13 13:46:23 +0200

    Update setPlay.jsx

--- studentily_app_frontend/frontend/src/components/pomodoroTimer/setPlay.jsx
+++ studentily_app_frontend/frontend/src/components/pomodoroTimer/setPlay.jsx
@@ -5,7 +5,7 @@ import { faPlay } from "@fortawesome/free-solid-svg-icons";
 export default function setPlay({ onClick }) {
   return (
     <div onClick={onClick}>
-      <button tabindex="2">
+      <button tabindex="3">
         <div className="flex justify-center items-center w-16 h-16 bg-gray-800 text-white rounded-[100%] p-4">
           <FontAwesomeIcon className="w-8 h-8 ml-1" icon={faPlay} />
         </div>
