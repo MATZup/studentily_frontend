7bc4efcc47acc4cc9fe8a267d265c15e3f7c2b9c
Matthias <66430209+MATZup@users.noreply.github.com>
2024-08-14 21:07:37 +0200

    Update notesPage.jsx

--- studentily_app_frontend/frontend/src/components/pages/notesPage.jsx
+++ studentily_app_frontend/frontend/src/components/pages/notesPage.jsx
@@ -102,7 +102,7 @@ export default function NotesPage() {
     <>
       <img
         src={notesBgEl}
-        alt=""
+        alt="notes background-element"
         className="fixed -bottom-1 -right-[38rem] max-960:w-[79rem] max-767:w-[74rem] max-650:w-[70rem] max-465:w-[64rem] max-385:w-[61rem] max-350:w-[59rem] z-[-50] max-w-[84rem] overflow-hidden pl-16 pr-16 aspect-[16/9]"
       />
 
