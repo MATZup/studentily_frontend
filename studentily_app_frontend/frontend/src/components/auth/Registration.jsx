759c6c369a740c082c2c960d7ea53e52e8c074f9
Matthias <66430209+MATZup@users.noreply.github.com>
2024-08-14 20:49:40 +0200

    Update Registration.jsx

--- studentily_app_frontend/frontend/src/components/auth/Registration.jsx
+++ studentily_app_frontend/frontend/src/components/auth/Registration.jsx
@@ -79,7 +79,7 @@ export default function Registration({ setIsAuthenticated }) {
       <div className="flex flex-col items-center h-[100vh] w-[100vw] justify-center">
         <img
           src={loginRegisterBgEl}
-          alt=""
+          alt="login and registration background-element"
           className="fixed -bottom-[2rem] -left-[24rem] max-1250:w-[82.5rem] max-960:w-[75rem] max-730:w-[58rem] max-650:w-[56rem] max-580:w-[56rem] max-500:w-[55.5rem] max-385:w-[51rem] z-[-50] max-w-[63rem] overflow-hidden pl-16 pr-16"
         />
 
