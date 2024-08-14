cc1cf80cc60fc7e6b2db84c9eb072f7fcae8e2ce
Matthias <66430209+MATZup@users.noreply.github.com>
2024-08-14 21:14:40 +0200

    Update todosPage.jsx

--- studentily_app_frontend/frontend/src/components/pages/todosPage.jsx
+++ studentily_app_frontend/frontend/src/components/pages/todosPage.jsx
@@ -253,7 +253,7 @@ export default function TodosPage() {
             </div>
           ) : (
             <div className="flex max-500:mt-2 flex-col justify-center items-center mx-auto mt-12">
-              <img className="w-72" src={todosEmoji} alt="" />
+              <img className="w-72" src={todosEmoji} alt="todos-mascot" />
               <span className="-mt-2 text-center">
                 Create <b>Todos!</b> Just click the <b>Create Todo</b>{" "}
                 Field below. <br />
