18b529b0efc2129ce1b1d44e2fdbbdbf1302bc6d
Matthias <66430209+MATZup@users.noreply.github.com>
2024-08-14 21:11:45 +0200

    Update pomodoroBg.jsx

--- studentily_app_frontend/frontend/src/components/pomodoroTimer/pomodoroBg.jsx
+++ studentily_app_frontend/frontend/src/components/pomodoroTimer/pomodoroBg.jsx
@@ -7,7 +7,7 @@ export default function pomodoroBg() {
       <img
         className="fixed max-960:w-[70rem] max-767:w-[68rem] max-650:w-[67rem] max-465:w-[64rem] max-385:w-[61rem] max-350:w-[59rem] max-w-[84rem] px-14 -bottom-8 z-[-50] w-[71rem] overflow-y-hidden"
         src={pomodoroBgImg}
-        alt=""
+        alt="pomodoro background-element"
       />
     </div>
   );
