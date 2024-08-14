38dc57ee4ab1bb336939f097426f93ca75548e64
Matthias <66430209+MATZup@users.noreply.github.com>
2024-08-14 21:02:57 +0200

    Update headerSection.jsx

--- studentily_app_frontend/frontend/src/components/home/headerSection.jsx
+++ studentily_app_frontend/frontend/src/components/home/headerSection.jsx
@@ -59,7 +59,7 @@ export default function HeaderSection() {
           <img
             className="max-500:-top-[3.8rem] max-500:-left-[1.2rem] max-500:w-[5.1rem] max-500:h-[4.1rem] max-767:-mt-[.55rem] max-767:w-[5.5rem] max-767:h-[4.4rem] max-786:absolute max-786:-left-5 max-786:-top-16 w-24 h-[4.8rem] -mt-3"
             src={uniSmiley}
-            alt="App Smiley"
+            alt="small main-mascot"
           />
         </h1>
         {!isSmallScreen && (
